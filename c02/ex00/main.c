39a8b2b868d33b4046d512dec5b3dea58b7d2450
fgroenew <fgroenew@student.codam.nl>
2024-07-13 12:40:34 +0200

    Created test for c02 ex00

---
+++ c02/ex00/main.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fgroenew <fgroenew@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/11 14:36:09 by fgroenew          #+#    #+#             */
+/*   Updated: 2024/07/13 12:39:29 by fgroenew         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+char	*ft_strcpy(char *dest, char *src);
+
+int	ft_contains_null_byte(char *str, int size)
+{
+	if (str[size] == '\0')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+void	ft_fill_str(char *str, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		str[i] = 'a';
+		i++;
+	}
+}
+
+void	test_00(void)
+{
+	char	dest[20];
+	char	src[14] = "Hello, world!";
+
+	ft_fill_str(dest, 20);
+	ft_strcpy(dest, src);
+	printf("--- EX00 - Test00\n");
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+	printf("contains null byte: %d\n", ft_contains_null_byte(dest, strlen(src)));
+}
+
+void	test_01(void)
+{
+	char	dest[20];
+	char	src[7] = "Hello!";
+
+	ft_fill_str(dest, 20);
+	ft_strcpy(dest, src);
+	printf("--- EX00 - Test01\n");
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+	printf("contains null byte: %d\n", ft_contains_null_byte(dest, strlen(src)));
+}
+
+int	main(void)
+{
+	test_00();
+	test_01();
+}
