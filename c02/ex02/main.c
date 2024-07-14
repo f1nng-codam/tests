90b894e17820ad45cbcaba35dec397cd1eec395f
Quinten Van Der Laan <quinten1vanderlaan@outlook.com>
2024-07-14 15:04:25 +0200

    added empty check in c02, ex02

--- c02/ex02/main.c
+++ c02/ex02/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgroenew <fgroenew@student.codam.nl>       +#+  +:+       +#+        */
+/*   By: qvan-der <qvan-der@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 12:49:08 by fgroenew          #+#    #+#             */
-/*   Updated: 2024/07/13 12:54:23 by fgroenew         ###   ########.fr       */
+/*   Updated: 2024/07/14 15:01:39 by qvan-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,18 @@ void	test_01(void)
 	printf("result: %d (should be 0)\n", ft_str_is_alpha(str));
 }
 
+void	test_02(void)
+{
+	char	str[1] = "";
+
+	printf("--- EX02 - Test02\n");
+	printf("str: %s\n", str);
+	printf("result: %d (should be 1)\n", ft_str_is_alpha(str));
+}
+
 int	main(void)
 {
 	test_00();
 	test_01();
+	test_02();
 }
