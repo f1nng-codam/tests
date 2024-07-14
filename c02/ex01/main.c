652d3973f25f332e8c80747203b629b0c7a91153
fgroenew <fgroenew@student.codam.nl>
2024-07-14 11:04:04 +0200

    Updated tests for c02 ex01

--- c02/ex01/main.c
+++ c02/ex01/main.c
@@ -6,7 +6,7 @@
 /*   By: fgroenew <fgroenew@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/11 14:36:09 by fgroenew          #+#    #+#             */
-/*   Updated: 2024/07/13 14:17:20 by fgroenew         ###   ########.fr       */
+/*   Updated: 2024/07/14 11:03:02 by fgroenew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n);
 
-int	ft_contains_null_byte(char *str, int size)
+int	ft_contains_null_bytes(char *str, int size_start, int size_end)
 {
-	if (str[size] == '\0')
+	while (size_start < size_end)
 	{
-		return (1);
+		if (str[size_start] != '\0')
+		{
+			return (0);
+		}
+		size_start++;
 	}
-	return (0);
+	return (1);
 }
 
 void	ft_fill_str(char *str, int size)
@@ -40,7 +44,7 @@ void	test_00(void)
 {
 	char	dest[20];
 	char	src[14] = "Hello, world!";
-	int		n = 13;
+	int		n = 20;
 
 	ft_fill_str(dest, 20);
 	ft_strncpy(dest, src, n);
@@ -48,22 +52,22 @@ void	test_00(void)
 	printf("src: %s\n", src);
 	printf("n: %d\n", n);
 	printf("dest: %s (should be Hello, world!)\n", dest);
-	printf("contains null byte: %d (should be 1)\n", ft_contains_null_byte(dest, n));
+	printf("contains null bytes: %d (should be 1)\n", ft_contains_null_bytes(dest, strlen(src), n));
 }
 
 void	test_01(void)
 {
-	char	dest[20];
+	char	dest[10];
 	char	src[7] = "Hello!";
 	int		n = 3;
 
-	ft_fill_str(dest, 20);
+	ft_fill_str(dest, 10);
 	ft_strncpy(dest, src, n);
 	printf("--- EX01 - Test01\n");
 	printf("src: %s\n", src);
 	printf("n: %d\n", n);
-	printf("dest: %s (should be Hel)\n", dest);
-	printf("contains null byte: %d (should be 1)\n", ft_contains_null_byte(dest, n));
+	printf("dest: %s (should be Helaaaaaaa, this can contain more characters after Helaaaaaaa)\n", dest);
+	printf("contains null bytes: %d (should be 1)\n", ft_contains_null_bytes(dest, strlen(src), n));
 }
 
 int	main(void)
