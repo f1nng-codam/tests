20f7727296420352064dd90a664fed0576654bfc
Quinten Van Der Laan <quinten1vanderlaan@outlook.com>
2024-07-15 12:55:44 +0200

    created test for ex11

---
+++ c02/ex11/main.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qvan-der <qvan-der@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/15 12:40:05 by qvan-der          #+#    #+#             */
+/*   Updated: 2024/07/15 12:52:36 by qvan-der         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_putstr_non_printable(char *str);
+
+void	test_00(void)
+{
+	char	str[] = "Coucou\ntu vas bien ?";
+
+	printf("--- EX07 - Test00\n");
+	printf("str: %s\n", str);
+	printf("result (should be 'Coucou\\0atu vas bien ?'):\n");
+	ft_putstr_non_printable(str);
+}
+
+void	test_01(void)
+{
+	char	str[] = "Hello\x01World\x02!";
+
+	printf("--- EX07 - Test01\n");
+	printf("str: %s\n", str);
+	printf("result (should be 'Hello\\01World\\02!'):\n");
+	ft_putstr_non_printable(str);
+}
+
+void	test_02(void)
+{
+	char	str[] = "Example with \x1F some non-printable \x7F chars.";
+
+	printf("--- EX07 - Test02\n");
+	printf("str: %s\n", str);
+	printf("result (should be 'Example with \\1f some non-printable \\7f chars.'):\n");
+	ft_putstr_non_printable(str);
+}
+
+int	main(void)
+{
+	test_00();
+	test_01();
+	test_02();
+}
