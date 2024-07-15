78c5750d2e67dd74b84dd2aa517067eb248a1c0f
Quinten Van Der Laan <quinten1vanderlaan@outlook.com>
2024-07-15 22:00:06 +0200

    added return tests for C02, ex05

--- c03/ex05/main.c
+++ c03/ex05/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgroenew <fgroenew@student.codam.nl>       +#+  +:+       +#+        */
+/*   By: qvan-der <qvan-der@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 12:33:53 by fgroenew          #+#    #+#             */
-/*   Updated: 2024/07/14 15:39:48 by fgroenew         ###   ########.fr       */
+/*   Updated: 2024/07/15 21:56:21 by qvan-der         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,16 @@ void	test_00(void)
 	char	dest[20] = "Hello";
 	char	src[] = ", world!";
 	int		size = 20;
+	unsigned int 	result;
 
 	printf("--- EX02 - Test00\n");
 	printf("dest: %s\n", dest);
 	printf("src: %s\n", src);
 	printf("size: %d\n", size);
 	ft_strlcat(dest, src, size);
+	result = ft_strlcat(dest, src, size);
 	printf("result: %s (should be Hello, world!)\n", dest);
+	printf("return: %d (should be 13)\n", result);
 }
 
 void	test_01(void)
@@ -33,13 +36,15 @@ void	test_01(void)
 	char	dest[20] = "Hello";
 	char	src[] = ", world!";
 	int		size = 10;
+	unsigned int 	result;
 
 	printf("--- EX02 - Test01\n");
 	printf("dest: %s\n", dest);
 	printf("src: %s\n", src);
 	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
+	result = ft_strlcat(dest, src, size);
 	printf("result: %s (should be Hello, wo)\n", dest);
+	printf("return: %d (should be 13)\n", result);
 }
 
 int	main(void)
