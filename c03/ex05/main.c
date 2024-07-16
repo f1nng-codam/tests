191e901e7bc96de74012e18c4c6b5f0c9cad9d96
fgroenew <fgroenew@student.codam.nl>
2024-07-16 12:46:59 +0200

    Fixed tests for c03 ex05

--- c03/ex05/main.c
+++ c03/ex05/main.c
@@ -6,7 +6,7 @@
 /*   By: fgroenew <fgroenew@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 12:33:53 by fgroenew          #+#    #+#             */
-/*   Updated: 2024/07/16 09:51:14 by fgroenew         ###   ########.fr       */
+/*   Updated: 2024/07/16 10:47:27 by fgroenew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,104 +45,6 @@ void	test_01(void)
 	result = ft_strlcat(dest, src, size);
 	printf("result: %s (should be Hello, wo)\n", dest);
 	printf("return: %d (should be 13)\n", result);
-}
-
-void	test_02(void)
-{
-	char	dest[20] = "";
-	char	src[] = "Hello, world!";
-	int		size = 20;
-
-	printf("--- EX02 - Test02\n");
-	printf("dest: %s\n", dest);
-	printf("src: %s\n", src);
-	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
-	printf("result: %s (should be Hello, world!)\n", dest);
-}
-
-void	test_03(void)
-{
-	char	dest[20] = "Hello";
-	char	src[] = "";
-	int		size = 20;
-
-	printf("--- EX02 - Test03\n");
-	printf("dest: %s\n", dest);
-	printf("src: %s\n", src);
-	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
-	printf("result: %s (should be Hello)\n", dest);
-}
-
-void	test_04(void)
-{
-	char	dest[20] = "Hello";
-	char	src[] = " world!";
-	int		size = 0;
-
-	printf("--- EX02 - Test04\n");
-	printf("dest: %s\n", dest);
-	printf("src: %s\n", src);
-	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
-	printf("result: %s (should be Hello)\n", dest);
-}
-
-void	test_05(void)
-{
-	char	dest[20] = "Hello";
-	char	src[] = " world!";
-	int		size = 5;
-
-	printf("--- EX02 - Test05\n");
-	printf("dest: %s\n", dest);
-	printf("src: %s\n", src);
-	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
-	printf("result: %s (should be Hello)\n", dest);
-}
-
-void	test_06(void)
-{
-	char	dest[20] = "Hello";
-	char	src[] = ", world!";
-	int		size = 13;
-
-	printf("--- EX02 - Test06\n");
-	printf("dest: %s\n", dest);
-	printf("src: %s\n", src);
-	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
-	printf("result: %s (should be Hello, world!)\n", dest);
-}
-
-void	test_07(void)
-{
-	char	dest[20] = "Hello";
-	char	src[] = ", world!";
-	int		size = 6;
-
-	printf("--- EX02 - Test07\n");
-	printf("dest: %s\n", dest);
-	printf("src: %s\n", src);
-	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
-	printf("result: %s (should be Hello)\n", dest);
-}
-
-void	test_08(void)
-{
-	char	dest[20] = "Hello";
-	char	src[] = ", world!";
-	int		size = 11;
-
-	printf("--- EX02 - Test08\n");
-	printf("dest: %s\n", dest);
-	printf("src: %s\n", src);
-	printf("size: %d\n", size);
-	ft_strlcat(dest, src, size);
-	printf("result: %s (should be Hello, wo)\n", dest);
 }
 
 void	test_02(void)
