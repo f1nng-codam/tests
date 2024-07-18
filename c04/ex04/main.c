2c266e294bb1fb8122f3ba1ab86672665c74f4dd
fgroenew <fgroenew@student.codam.nl>
2024-07-18 11:52:32 +0200

    Updated tests for c04 ex02 & ex04

--- c04/ex04/main.c
+++ c04/ex04/main.c
@@ -6,7 +6,7 @@
 /*   By: fgroenew <fgroenew@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 16:41:16 by fgroenew          #+#    #+#             */
-/*   Updated: 2024/07/15 16:00:08 by fgroenew         ###   ########.fr       */
+/*   Updated: 2024/07/18 11:48:26 by fgroenew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,6 +149,36 @@ void	test_08(void)
 	printf("\n");
 }
 
+void	test_09(void)
+{
+	int		nbr;
+	char	*base;
+
+	nbr = -2147483648;
+	base = "0123456789ABCDEF";
+	printf("--- EX04 - Test09\n");
+	printf("nbr: %d\n", nbr);
+	printf("base: %s\n", base);
+	printf("output (should be -80000000)\n");
+	ft_putnbr_base(nbr, base);
+	printf("\n");
+}
+
+void	test_10(void)
+{
+	int		nbr;
+	char	*base;
+
+	nbr = -2147483648;
+	base = "poneyvif";
+	printf("--- EX04 - Test010\n");
+	printf("nbr: %d\n", nbr);
+	printf("base: %s\n", base);
+	printf("output (should be -npppppppppp)\n");
+	ft_putnbr_base(nbr, base);
+	printf("\n");
+}
+
 int	main(void)
 {
 	test_00();
@@ -160,4 +190,6 @@ int	main(void)
 	test_06();
 	test_07();
 	test_08();
+	test_09();
+	test_10();
 }
