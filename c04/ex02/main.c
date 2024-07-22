f8069216d95f5fd600d0743d04e621bed0124f48
fgroenew <fgroenew@student.codam.nl>
2024-07-22 09:40:35 +0200

    Updated tests for c04 ex02

--- c04/ex02/main.c
+++ c04/ex02/main.c
@@ -6,7 +6,7 @@
 /*   By: fgroenew <fgroenew@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/14 16:41:16 by fgroenew          #+#    #+#             */
-/*   Updated: 2024/07/18 11:18:53 by fgroenew         ###   ########.fr       */
+/*   Updated: 2024/07/22 09:35:39 by fgroenew         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,28 @@ void	test_04(void)
 	printf("\n");
 }
 
+void	test_05(void)
+{
+	int	nb;
+
+	nb = 2147483647;
+	printf("--- EX02 - Test05\n");
+	printf("output (should be 2147483647)\n");
+	ft_putnbr(nb);
+	printf("\n");
+}
+
+void	test_06(void)
+{
+	int	nb;
+
+	nb = -2147483648;
+	printf("--- EX02 - Test06\n");
+	printf("output (should be -2147483648)\n");
+	ft_putnbr(nb);
+	printf("\n");
+}
+
 int	main(void)
 {
 	test_00();
@@ -76,4 +98,6 @@ int	main(void)
 	test_02();
 	test_03();
 	test_04();
+	test_05();
+	test_06();
 }
